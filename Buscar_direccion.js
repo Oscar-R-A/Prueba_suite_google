3b100779b31906a67ea6a046678f396e067de90c
Oscar R <ogra.07@hotmail.com>
2021-09-06 23:12:39 -0500

    Cambio en el assert

--- Buscar_direccion.js
+++ Buscar_direccion.js
@@ -15,7 +15,7 @@ describe('Buscar una dirección desde el navegador', function(){
     it('Ingresar dirección de origen', function(){
         cy.get('#sb_ifc51 > .tactile-searchbox-input').type('Cinte bogotá {Enter}');
     })
-    it('Assert de que se realizo la busqueda bien', function(){
+    it('Assert de que se realizo la busqueda de manera correcta', function(){
         expect(cy.get('.widget-scene-canvas')).to.exist;
     })
 })
