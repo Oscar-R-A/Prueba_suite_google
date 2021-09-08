302aa1f638e5dee52332e15f631786005facd6ff
Oscar R <ogra.07@hotmail.com>
2021-09-07 22:56:09 -0500

    Add Youtube

---
+++ Video_youtube.js
@@ -0,0 +1,10 @@
+describe('Abrir y colocar un video en youtube', function(){
+    it('Abrir google', function(){
+        cy.visit('https://www.google.com/');
+    })
+    it('Buscar Youtube testing y automatización', function(){
+        cy.get('.gLFyf').type('Youtube testing y automatización{Enter}');
+        cy.get('[data-hveid="CBgQAA"] > .tF2Cxc > .yuRUbf > a > .TbwUpd > .iUh30').click();
+        cy.wait(5000);
+    })
+})
